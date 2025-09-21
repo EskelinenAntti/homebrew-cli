9e0fa856513a67d488eb687a2b3608c65aaf187f
Antti Eskelinen <29664167+EskelinenAntti@users.noreply.github.com>
2025-09-21 10:24:17 +0300

    bump version

--- Formula/boring.ide.rb
+++ Formula/boring.ide.rb
@@ -1,7 +1,7 @@
 class BoringIde < Formula
   desc "Helix + lazygit + tmux = boring.ide"
   homepage "https://github.com/boringconfigs/boring.ide"
-  url "https://github.com/boringconfigs/boring.ide/archive/refs/tags/v0.0.2.tar.gz"
+  url "https://api.github.com/repos/boringconfigs/boring.ide/tarball/v0.0.3"
   sha256 "8c304db593e30915af3b6c5a133a05b72b4a3ca67dc040ea31adae102e770bb1"
   license ""
 
@@ -14,6 +14,6 @@ class BoringIde < Formula
   end
 
   test do
-    assert_match "Usage: ide [path]", shell_output("#{bin}/ide --help")
+    assert_match "Usage: $(basename $0) [path]\n\nArguments\n    path (optional) - Path to project root directory.\n\n", shell_output("#{bin}/ide --help")
   end
 end
