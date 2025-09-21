1cef419d0710246b6ea4319471863548b915722a
Antti Eskelinen <29664167+EskelinenAntti@users.noreply.github.com>
2025-09-21 08:44:17 +0300

    Fix CI errors

--- Formula/boring.ide.rb
+++ Formula/boring.ide.rb
@@ -1,13 +1,13 @@
 class BoringIde < Formula
-  desc "helix + lazygit + fzf + tmux = boring.ide"
+  desc "Helix + lazygit + tmux = boring.ide. It's recommended to add fzf to the mix."
   homepage "https://github.com/boringconfigs/boring.ide"
   url "https://github.com/boringconfigs/boring.ide/archive/refs/tags/v0.0.1.tar.gz"
   sha256 "85772d34f12a7fe9b5264072b23388dc017cd2693513548948193ed765ab69fc"
   license ""
 
   depends_on "helix"
-  depends_on "tmux"
   depends_on "lazygit"
+  depends_on "tmux"
 
   def install
     bin.install "bin/ide" => "ide"
