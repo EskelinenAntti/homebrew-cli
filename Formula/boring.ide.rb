d5c911426651703d9ec9a4c04000751fbd0be054
Antti Eskelinen <29664167+EskelinenAntti@users.noreply.github.com>
2025-09-21 08:46:06 +0300

    Trying to please the CI

--- Formula/boring.ide.rb
+++ Formula/boring.ide.rb
@@ -1,5 +1,5 @@
 class BoringIde < Formula
-  desc "Helix + lazygit + tmux = boring.ide. It's recommended to add fzf to the mix."
+  desc "Helix + lazygit + tmux = boring.ide"
   homepage "https://github.com/boringconfigs/boring.ide"
   url "https://github.com/boringconfigs/boring.ide/archive/refs/tags/v0.0.1.tar.gz"
   sha256 "85772d34f12a7fe9b5264072b23388dc017cd2693513548948193ed765ab69fc"
