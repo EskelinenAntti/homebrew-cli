5a7de9a457a5791847ed1eae39886b931b0354f9
Antti Eskelinen <29664167+EskelinenAntti@users.noreply.github.com>
2025-09-21 10:28:25 +0300

    fix sha

--- Formula/boring.ide.rb
+++ Formula/boring.ide.rb
@@ -2,7 +2,7 @@ class BoringIde < Formula
   desc "Helix + lazygit + tmux = boring.ide"
   homepage "https://github.com/boringconfigs/boring.ide"
   url "https://github.com/boringconfigs/boring.ide/archive/refs/tags/v0.0.3.tar.gz"
-  sha256 "8c304db593e30915af3b6c5a133a05b72b4a3ca67dc040ea31adae102e770bb1"
+  sha256 "84cd154a789bce45ef950804ad38fe3191479122783f25e96743a34eaa4a97c4"
   license ""
 
   depends_on "helix"
