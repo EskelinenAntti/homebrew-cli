6cf754a18e183c0660c37d5fabcc942f70bb369a
Antti Eskelinen <29664167+EskelinenAntti@users.noreply.github.com>
2025-09-27 20:27:34 +0300

    rename stuff

--- Formula/tmux-ide.rb
+++ Formula/tmux-ide.rb
@@ -1,6 +1,6 @@
 class BoringIde < Formula
-  desc "Helix + lazygit + tmux = boring.ide"
-  homepage "https://github.com/eskelinenantti/boring.ide"
+  desc "Helix + lazygit + tmux = tmux-ide"
+  homepage "https://github.com/eskelinenantti/tmux-ide"
   url "https://github.com/eskelinenantti/tmux-ide/archive/refs/tags/v0.0.6.tar.gz"
   sha256 "06115943e7cae0bd259a0b9527b0c12b4054fe8474d97f970d0dfb3d96bee39f"
   license ""
