64085409265d3b931f0f0e34b548f1c4212aa9d4
Antti Eskelinen <29664167+EskelinenAntti@users.noreply.github.com>
2025-10-03 21:18:57 +0300

    homebrew-automation 0.0.3

--- Formula/homebrew-automation.rb
+++ Formula/homebrew-automation.rb
@@ -1,8 +1,8 @@
 class HomebrewAutomation < Formula
   desc "Handy scripts for automating Homebrew related actions"
   homepage "https://github.com/EskelinenAntti/homebrew-automation"
-  url "https://github.com/EskelinenAntti/homebrew-automation/archive/refs/tags/v0.0.2.tar.gz"
-  sha256 "646261f25c41ee15236cee93758eb4daa293afddc7dbe5865eb8dbd758cc277e"
+  url "https://github.com/EskelinenAntti/homebrew-automation/archive/refs/tags/v0.0.3.tar.gz"
+  sha256 "6ac504f53cf5ba31503210466a58947f8328d89fe76b6d833861e0b193cb2360"
   license ""
 
   def install
