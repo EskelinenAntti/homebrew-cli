af23cb3bc7f0d5e02932f4b995379d8ea40d1915
Antti Eskelinen <29664167+EskelinenAntti@users.noreply.github.com>
2025-10-25 18:10:04 +0000

    tmuxide: add 1.0.1 bottle.

--- Formula/tmuxide.rb
+++ Formula/tmuxide.rb
@@ -5,6 +5,12 @@ class Tmuxide < Formula
   sha256 "61a9cb80a9b2a76c2d83ef65580714ba659aba48c48fc83200f373dd92dcc4a1"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/EskelinenAntti/homebrew-cli/releases/download/tmuxide-1.0.1"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "497a058d4ab6b501aec83417a78521ce890e066e97a2d0ee71696e6166d8ce7b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "51608623149ad3c79056a26d7c491df288b33a6a5d52ac4fd4af90a0bfa70477"
+  end
+
   depends_on "go" => :build
 
   def install
