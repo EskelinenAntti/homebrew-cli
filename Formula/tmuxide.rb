479659423d945c6ddae0059dd3b00ce18e23b2be
Antti Eskelinen <29664167+EskelinenAntti@users.noreply.github.com>
2025-10-25 21:22:46 +0300

    tmuxide 1.0.2
    Closes #17.
    
    Signed-off-by: Antti Eskelinen <29664167+EskelinenAntti@users.noreply.github.com>

--- Formula/tmuxide.rb
+++ Formula/tmuxide.rb
@@ -1,8 +1,8 @@
 class Tmuxide < Formula
   desc "Turn your terminal into an ide"
   homepage "https://github.com/EskelinenAntti/tmuxide"
-  url "https://github.com/EskelinenAntti/tmuxide/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "61a9cb80a9b2a76c2d83ef65580714ba659aba48c48fc83200f373dd92dcc4a1"
+  url "https://github.com/EskelinenAntti/tmuxide/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "8e7ffb588cfa7df14ba439cac884674335d1d66e540f964be7bf6093505868c1"
   license "MIT"
 
   bottle do
