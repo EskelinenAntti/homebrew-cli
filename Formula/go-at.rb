7889a00e6277c0522762ca15af8e8ac0a9fedad9
Antti Eskelinen <29664167+EskelinenAntti@users.noreply.github.com>
2025-11-12 19:38:55 +0200

    fix description

--- Formula/go-at.rb
+++ Formula/go-at.rb
@@ -1,5 +1,5 @@
 class GoAt < Formula
-  desc "Open or create tmux sessions at any directory "
+  desc "Open or create tmux sessions at any directory"
   homepage "https://github.com/EskelinenAntti/go-at"
   url "https://github.com/EskelinenAntti/go-at/archive/refs/tags/v0.0.1.tar.gz"
   sha256 "7c7c69607a9a0636dd69752747fe3b919f9e49b1c5f77cb49c11c8d8572acf28"
