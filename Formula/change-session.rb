24e79afd8e50f9a13bdb5c872373174cdf90a766
Antti Eskelinen <29664167+EskelinenAntti@users.noreply.github.com>
2025-11-15 09:23:48 +0200

    lint

--- Formula/change-session.rb
+++ Formula/change-session.rb
@@ -1,5 +1,5 @@
 class ChangeSession < Formula
-  desc "Open or create tmux sessions at any directory "
+  desc "Open or create tmux sessions at any directory"
   homepage "https://github.com/EskelinenAntti/change-session"
   url "https://github.com/EskelinenAntti/change-session/archive/refs/tags/v0.0.1.tar.gz"
   sha256 "aee0cd22825154fef4d5cee2ef0cf366b75c81bb46c29383dcbf7c24d55dbe3a"
