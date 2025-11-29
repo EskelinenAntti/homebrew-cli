921957e41aa71aad54190943e4c0059c935dbf7d
Antti Eskelinen <29664167+EskelinenAntti@users.noreply.github.com>
2025-11-29 11:16:29 +0000

    tmuxide: update 1.1.3 bottle.

--- Formula/tmuxide.rb
+++ Formula/tmuxide.rb
@@ -6,9 +6,9 @@ class Tmuxide < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/EskelinenAntti/homebrew-cli/releases/download/tmuxide-1.1.0"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "180ba50664eb5e9bcc4ad7eca49609628d5fdd020e5107d25c93bf793a295f43"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6d3941ffa1c732ee62360b523d0c6d6c5d59961251b865af8178e31877d39185"
+    root_url "https://github.com/EskelinenAntti/homebrew-cli/releases/download/tmuxide-1.1.3"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "355e284cfb9fd4c4f3c27ccbd2b80ed7d10fe448dff65a5b80f4bbac929bc0e3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cc9b92f0027a9e6920e31b5c2cc252a2d701b8524dfaee46ee52a43a4b8f2770"
   end
 
   depends_on "go" => :build
