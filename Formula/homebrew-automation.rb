37e2a1cd54828ae7096b1e5ef05de06df4ddca04
Antti Eskelinen <antti.eskelinen.dev@gmail.com>
2026-01-24 10:40:59 +0200

    homebrew-automation 0.3.1
    Closes #42.
    
    Signed-off-by: Antti Eskelinen <antti.eskelinen.dev@gmail.com>

--- Formula/homebrew-automation.rb
+++ Formula/homebrew-automation.rb
@@ -1,8 +1,8 @@
 class HomebrewAutomation < Formula
   desc "Handy scripts for automating Homebrew related actions"
   homepage "https://github.com/EskelinenAntti/homebrew-automation"
-  url "https://github.com/EskelinenAntti/homebrew-automation/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "54811d04767e861d2a74f4e12f4ecf0d683d76887ad51cf8247a735a19fbd803"
+  url "https://github.com/EskelinenAntti/homebrew-automation/archive/refs/tags/v0.3.1.tar.gz"
+  sha256 "103f9f2629d38a323fd3ef0e879879d3413c03ce25e5446e2b86773dbf497150"
   license ""
 
   bottle do
