6cff0ed953c98b67fdfb969cc6108ab7e337a145
Antti Eskelinen <antti.eskelinen.dev@gmail.com>
2026-02-28 09:59:53 +0000

    homebrew-automation: update 0.3.3 bottle.

--- Formula/homebrew-automation.rb
+++ Formula/homebrew-automation.rb
@@ -6,10 +6,10 @@ class HomebrewAutomation < Formula
   license ""
 
   bottle do
-    root_url "https://github.com/EskelinenAntti/homebrew-cli/releases/download/homebrew-automation-0.3.2"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c53c113d0af2232877b4981882bc36b12f9ef5fe0f1539639634356448aa1ba8"
-    sha256 cellar: :any_skip_relocation, sequoia:       "7fad0a2864c4cf8ccbd2c933a2108e3ad484c92b5336da4a49ea84fe1bddcfd2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cf1d38fdf3392f1657530b49b3b1b4d4b0464eb842bd8f1b2461d0281c44f194"
+    root_url "https://github.com/EskelinenAntti/homebrew-cli/releases/download/homebrew-automation-0.3.3"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "26dc8156369c208547b95aa2c7baabd788049fba2426214f1938b65c2157478d"
+    sha256 cellar: :any_skip_relocation, sequoia:       "1417ca5c02c7a849993b40bdc5bf82c3f597648437c4af24afd59c4eeaa0d4c2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "28869d795c4252004f43d2f0454916e539b87bcb98532ef3e6dac5bddd9801f0"
   end
 
   def install
