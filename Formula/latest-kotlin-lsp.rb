747778327b1b9bb4ac0de00bc28fdb78d8fb59cd
Antti Eskelinen <antti.eskelinen.dev@gmail.com>
2026-04-30 22:13:28 +0300

    fix

---
+++ Formula/latest-kotlin-lsp.rb
@@ -0,0 +1,23 @@
+cask "latest-kotlin-lsp" do
+  version "262.4739.0"
+
+  on_arm do
+    sha256 "1b745743ce22ad92681a1bc3b1046803e942a6e1f36e04fb85ae9a40334a2f1e"
+
+    url "https://download-cdn.jetbrains.com/kotlin-lsp/#{version}/kotlin-server-#{version}-aarch64.sit"
+  end
+
+  on_intel do
+    sha256 "6f06efe7a10f94b9c8a028c4efeb6c7e1769f47a01edfb74450acf30ab5665e4"
+
+    url "https://download-cdn.jetbrains.com/kotlin-lsp/#{version}/kotlin-server-#{version}-x64.zip"
+  end
+
+  name "Latest Kotlin LSP"
+  desc "Latest official Kotlin Language Server Protocol"
+  homepage "https://github.com/Kotlin/kotlin-lsp"
+
+  depends_on macos: ">= :ventura"
+
+  artifact "bin/intellij-server", target: "/usr/local/bin/intellij-server"
+end
