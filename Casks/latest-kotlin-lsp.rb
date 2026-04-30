46a796cb7b5166824704723071ca76459bf6475c
Antti Eskelinen <antti.eskelinen.dev@gmail.com>
2026-04-30 22:17:22 +0300

    no idea

--- Casks/latest-kotlin-lsp.rb
+++ Casks/latest-kotlin-lsp.rb
@@ -1,16 +1,19 @@
 cask "latest-kotlin-lsp" do
   version "262.4739.0"
-  sha256 arm:   "1b745743ce22ad92681a1bc3b1046803e942a6e1f36e04fb85ae9a40334a2f1e",
-         intel: "6f06efe7a10f94b9c8a028c4efeb6c7e1769f47a01edfb74450acf30ab5665e4"
 
-  url arm:   "https://download-cdn.jetbrains.com/kotlin-lsp/#{version}/kotlin-server-#{version}-aarch64.sit",
-      intel: "https://download-cdn.jetbrains.com/kotlin-lsp/#{version}/kotlin-server-#{version}-x64.zip"
+  on_arm do
+    url "https://download-cdn.jetbrains.com/kotlin-lsp/#{version}/kotlin-server-#{version}-aarch64.sit"
+    sha256 "1b745743ce22ad92681a1bc3b1046803e942a6e1f36e04fb85ae9a40334a2f1e"
+  end
+
+  on_intel do
+    url "https://download-cdn.jetbrains.com/kotlin-lsp/#{version}/kotlin-server-#{version}-x64.zip"
+    sha256 "6f06efe7a10f94b9c8a028c4efeb6c7e1769f47a01edfb74450acf30ab5665e4"
+  end
 
   name "Latest Kotlin LSP"
-  desc "Latest official Language Server Protocol for Kotlin"
+  desc "Kotlin LSP server"
   homepage "https://github.com/Kotlin/kotlin-lsp"
 
-  depends_on macos: ">= :ventura"
-
-  artifact "bin/intellij-server", target: "/usr/local/bin/intellij-server"
+  binary "bin/intellij-server", target: "intellij-server"
 end
