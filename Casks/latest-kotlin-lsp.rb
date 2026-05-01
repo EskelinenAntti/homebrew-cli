6e9ee97ee71423188cf0970391c33d94db8b940f
Antti Eskelinen <antti.eskelinen.dev@gmail.com>
2026-05-01 16:31:31 +0300

    fix sha

--- Casks/latest-kotlin-lsp.rb
+++ Casks/latest-kotlin-lsp.rb
@@ -2,13 +2,13 @@ cask "latest-kotlin-lsp" do
   version "262.4739.0"
 
   on_arm do
-    sha256 "46971110c9b8a3360ce3fdf5437467f4c447dad37ad73dbf81d64af6779e4105"
+    sha256 "625870ae091c6d0dee25514d545c708a6ea50d7cbb5154aaf1aa9123ccff338b"
 
     url "https://download-cdn.jetbrains.com/kotlin-lsp/#{version}/kotlin-server-#{version}-aarch64.tar.gz"
   end
 
   on_intel do
-    sha256 "625870ae091c6d0dee25514d545c708a6ea50d7cbb5154aaf1aa9123ccff338b"
+    sha256 "1b745743ce22ad92681a1bc3b1046803e942a6e1f36e04fb85ae9a40334a2f1e"
 
     url "https://download-cdn.jetbrains.com/kotlin-lsp/#{version}/kotlin-server-#{version}-x64.tar.gz"
   end
