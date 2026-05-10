adbf0ce5b4da989f87fb30e3a729879fddbdd043
Antti Eskelinen <antti.eskelinen.dev@gmail.com>
2026-05-10 16:53:43 +0300

    fix style errors

--- Casks/latest-kotlin-lsp.rb
+++ Casks/latest-kotlin-lsp.rb
@@ -6,7 +6,6 @@ cask "latest-kotlin-lsp" do
 
     url "https://download-cdn.jetbrains.com/kotlin-lsp/#{version}/kotlin-server-#{version}-aarch64.sit"
   end
-
   on_intel do
     sha256 "6f06efe7a10f94b9c8a028c4efeb6c7e1769f47a01edfb74450acf30ab5665e4"
 
@@ -23,8 +22,14 @@ cask "latest-kotlin-lsp" do
   binary "kotlin-server-#{version}/kotlin-lsp.sh", target: "kotlin-lsp"
 
   postflight do
-    system "xattr -d com.apple.quarantine #{HOMEBREW_PREFIX}/bin/intellij-server"
-    system "xattr -d com.apple.quarantine #{staged_path}/kotlin-server-#{version}/lib/filewatcher/#{Hardware::CPU.arm? ? "macOS-AArch64" : "macOS-X64"}/libfilewatcher_jni.dylib"
-    system "xattr -d com.apple.quarantine #{staged_path}/kotlin-server-#{version}/lib/rocksdbjni/librocksdbjni-osx-#{Hardware::CPU.arm? ? "arm64" : "x86_64"}.jnilib"
+    system "xattr", "-d", "com.apple.quarantine", "#{HOMEBREW_PREFIX}/bin/intellij-server"
+
+    filewatcher_arch = on_arch_conditional arm: "macOS-AArch64", intel: "macOS-X64"
+    system "xattr", "-d", "com.apple.quarantine",
+           "#{staged_path}/kotlin-server-#{version}/lib/filewatcher/#{filewatcher_arch}/libfilewatcher_jni.dylib"
+
+    rocksdb_arch = on_arch_conditional arm: "arm64", intel: "x86_64"
+    system "xattr", "-d", "com.apple.quarantine",
+           "#{staged_path}/kotlin-server-#{version}/lib/rocksdbjni/librocksdbjni-osx-#{rocksdb_arch}.jnilib"
   end
 end
