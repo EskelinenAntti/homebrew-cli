d13d914d442c13f0a9635ad53e2fa0c4ba762c16
Antti Eskelinen <antti.eskelinen.dev@gmail.com>
2026-06-10 20:51:28 +0300

    latest-kotlin-lsp formula

---
+++ Formula/latest-kotlin-lsp.rb
@@ -0,0 +1,40 @@
+class LatestKotlinLsp < Formula
+  desc "Official Language Server Protocol for the Kotlin language"
+  homepage "https://github.com/Kotlin/kotlin-lsp"
+  version "262.7569.0"
+  license "Apache-2.0"
+
+  livecheck do
+    url :homepage
+    strategy :github_latest
+  end
+
+  # We don't want to support brew packages on Linux (On Linux, people should be using their native package managers)
+  depends_on :macos
+
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://download-cdn.jetbrains.com/kotlin-lsp/#{version}/kotlin-server-#{version}.sit",
+          using: :nounzip
+      sha256 "6f06efe7a10f94b9c8a028c4efeb6c7e1769f47a01edfb74450acf30ab5665e4"
+    end
+    if Hardware::CPU.arm?
+      url "https://download-cdn.jetbrains.com/kotlin-lsp/#{version}/kotlin-server-#{version}-aarch64.sit",
+          using: :nounzip
+      sha256 "1b745743ce22ad92681a1bc3b1046803e942a6e1f36e04fb85ae9a40334a2f1e"
+    end
+  end
+
+  def install
+    archive_name = "kotlin-server-#{version}"
+    archive_name += "-aarch64" if Hardware::CPU.arm?
+
+    system "unzip", "-q", "#{archive_name}.sit"
+    libexec.install Dir["kotlin-server-#{version}/*"]
+    bin.install_symlink "#{libexec}/bin/intellij-server" => "kotlin-lsp"
+  end
+
+  test do
+    assert_match "LS-#{version}", shell_output("#{bin}/kotlin-lsp --version")
+  end
+end
